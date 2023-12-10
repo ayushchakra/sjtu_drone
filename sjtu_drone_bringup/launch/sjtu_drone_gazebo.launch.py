361c8288570a563274769339ffdb5bc512ccb615
ayushchakra <achakraborty@olin.edu>
2023-12-10 16:28:39 -0500

    change drone world file

--- sjtu_drone_bringup/launch/sjtu_drone_gazebo.launch.py
+++ sjtu_drone_bringup/launch/sjtu_drone_gazebo.launch.py
@@ -11,14 +11,13 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 import xacro
 
-def generate_launch_description():
 
+def generate_launch_description():
     use_sim_time = LaunchConfiguration("use_sim_time", default="false")
     xacro_file_name = "sjtu_drone.urdf.xacro"
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_gazebo_ros = get_package_share_directory("gazebo_ros")
     xacro_file = os.path.join(
-        get_package_share_directory("sjtu_drone_description"),
-        "urdf", xacro_file_name
+        get_package_share_directory("sjtu_drone_description"), "urdf", xacro_file_name
     )
     robot_description_config = xacro.process_file(xacro_file)
     robot_desc = robot_description_config.toxml()
@@ -26,48 +25,51 @@ def generate_launch_description():
 
     world_file = os.path.join(
         get_package_share_directory("sjtu_drone_description"),
-        "worlds", "playground.world"
+        "worlds",
+        "cafe.world",
     )
 
-    return LaunchDescription([
-        Node(
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            name="robot_state_publisher",
-            # namespace=model_ns,
-            output="screen",
-            parameters=[{"use_sim_time": use_sim_time, "robot_description": robot_desc}],
-            arguments=[robot_desc]
-        ),
-
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            # namespace=model_ns,
-            output='screen',
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                name="robot_state_publisher",
+                # namespace=model_ns,
+                output="screen",
+                parameters=[
+                    {"use_sim_time": use_sim_time, "robot_description": robot_desc}
+                ],
+                arguments=[robot_desc],
             ),
-            launch_arguments={'world': world_file,
-                              'verbose': "true",
-                              'extra_gazebo_args': 'verbose'}.items()
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                # namespace=model_ns,
+                output="screen",
             ),
-            launch_arguments={'verbose': "true"}.items()
-        ),
-
-        Node(
-            package="sjtu_drone_bringup",
-            executable="spawn_drone",
-            arguments=[robot_desc, model_ns],
-            output="screen"
-        )
-    ])
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_gazebo_ros, "launch", "gzserver.launch.py")
+                ),
+                launch_arguments={
+                    "world": world_file,
+                    "verbose": "true",
+                    "extra_gazebo_args": "verbose",
+                }.items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_gazebo_ros, "launch", "gzclient.launch.py")
+                ),
+                launch_arguments={"verbose": "true"}.items(),
+            ),
+            Node(
+                package="sjtu_drone_bringup",
+                executable="spawn_drone",
+                arguments=[robot_desc, model_ns],
+                output="screen",
+            ),
+        ]
+    )
